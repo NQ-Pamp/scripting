1dcf18fa682d14922e6562c1d3ea4851f7b04c2a
mtoudert <mtoudert@novaquark.com>
2019-09-05 15:26:35 +0200

    started python refresh

---
+++ wut.py
@@ -0,0 +1,18 @@
+from math import *
+
+print("yo !")
+
+character_name = "pamp"
+character_age = 29
+
+print(character_name + " is " + str(character_age))
+
+my_num = 8
+
+print(str(my_num))
+print(max(5, 9))
+print(pow(4, 9))
+print(round(3.3))
+print(ceil(3.1))
+
+warriors = ["Tiny", "Axe", "Tusk"]
